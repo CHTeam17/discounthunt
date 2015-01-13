3e8f22849d2b62162ad5f4324f5cd68b8e8bcc36
Michael Oemler <michael.oemler@sap.com>
2015-01-13 10:07:19 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Load properties file
-. ../user.properties
+. ../user_team_east.properties
 
 export HTTP_PROXY=http://proxy.wdf.sap.corp:8080
-/usr/bin/cf login -u $user -p $password -o sap_demo -a http://api.cf1.hybris.com --skip-ssl-validation
+/usr/bin/cf login -u $user -p $password -o sap_demo -a http://api.cf3.hybris.com --skip-ssl-validation
 /usr/bin/cf push -f dev-manifest.yml
