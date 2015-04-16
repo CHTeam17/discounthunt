1a410a19184c3e368229177932ef8f66b1a3e4a4
Pierre <pierre.fritsch@sap.com>
2015-04-16 14:15:13 +0200

    Replace hardwired targets by variables in upload.sh

--- upload.sh
+++ upload.sh
@@ -4,5 +4,5 @@
 . ../user-team-sap-east.properties
 
 export HTTPS_PROXY=http://proxy.wdf.sap.corp:8080
-/usr/bin/cf login -u $user -p $password -o sap_cec_prod -a https://api.cf3.hybris.com --skip-ssl-validation
+/usr/bin/cf login -u $user -p $password -o $organization -a $target --skip-ssl-validation
 /usr/bin/cf push -f dev-manifest.yml
