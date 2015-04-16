c4b9f39bcad52e004087f90aeba9d01a64db8636
Pierre <pierre.fritsch@sap.com>
2015-04-16 14:01:07 +0200

    Use RAMLer instead of our own RestClient

--- src/main/java/com/sap/wishlist/service/WishlistService.java
+++ src/main/java/com/sap/wishlist/service/WishlistService.java
@@ -1,65 +1,116 @@
 package com.sap.wishlist.service;
 
-import java.net.URI;
-import java.util.ArrayList;
-
 import javax.annotation.ManagedBean;
 import javax.inject.Inject;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import com.hybris.api.DocumentRepositoryClient;
 import com.sap.wishlist.api.generated.AppAwareParameters;
 import com.sap.wishlist.api.generated.Wishlist;
-import com.sap.wishlist.client.WishlistDocumentClient;
-import com.sap.wishlist.client.document.DocumentWishlist;
-import com.sap.wishlist.client.document.ResponseCreated;
+import com.sap.wishlist.client.OAuth2ServiceClient;
 
 @ManagedBean
 public class WishlistService {
 
+    public static final String WISHLIST_PATH = "wishlist";
+
     @Inject
-    private WishlistDocumentClient documentClient;
+    private OAuth2ServiceClient oAuth2Client;
+
+    private String clientId;
+
+    @Value("${service.client_id}")
+    public void setClientId(String clientId) {
+	this.clientId = clientId;
+    }
 
     /* GET / */
-    public Response get(final AppAwareParameters appAware)
-    {
-	ArrayList<DocumentWishlist> wishlists = documentClient.getWishlists(appAware.getHybrisTenant());
+    public Response get(final AppAwareParameters appAware) {
+	String authorization = oAuth2Client.requestAccessToken(appAware.getHybrisTenant());
 
-	return Response.ok().entity(wishlists).build();
+	DocumentRepositoryClient client = new DocumentRepositoryClient(DocumentRepositoryClient.DEFAULT_BASE_URI);
+	final Response response = client.tenant(appAware.getHybrisTenant())
+		.appData(this.clientId)
+		.type(WISHLIST_PATH)
+		.prepareGet()
+		.withHeader("Authorization", authorization)
+		.execute();
+
+	return response;
     }
 
     /* POST / */
-    public Response post(final AppAwareParameters appAware, final UriInfo uriInfo, final Wishlist wishlist)
-    {
-	ResponseCreated wishlistDataType = documentClient.createWishlist(appAware.getHybrisTenant(), wishlist,
-		wishlist.getId());
+    public Response post(final AppAwareParameters appAware, final UriInfo uriInfo, final Wishlist wishlist) {
+	String wishlistId = wishlist.getId();
 
-	URI resourceURI = uriInfo.getRequestUriBuilder().path("/" + wishlistDataType.getId()).build();
-	return Response.created(resourceURI).build();
+	String authorization = oAuth2Client.requestAccessToken(appAware.getHybrisTenant());
+
+	DocumentRepositoryClient client = new DocumentRepositoryClient(DocumentRepositoryClient.DEFAULT_BASE_URI);
+	final Response response = client.tenant(appAware.getHybrisTenant())
+		.appData(this.clientId)
+		.type(WISHLIST_PATH)
+		.dataId(wishlistId)
+		.preparePost()
+		.withHeader("Authorization", authorization)
+		.withPayload(Entity.json(wishlist))
+		.execute();
+
+	return response;
     }
 
     /* GET //{wishlistId} */
-    public Response getByWishlistId(final AppAwareParameters appAware, final java.lang.String wishlistId)
-    {
-	DocumentWishlist documentWishlist = documentClient.getWishlist(appAware.getHybrisTenant(), wishlistId);
-	Wishlist wishlist = documentWishlist;
+    public Response getByWishlistId(final AppAwareParameters appAware, final java.lang.String wishlistId) {
+	String authorization = oAuth2Client.requestAccessToken(appAware.getHybrisTenant());
 
-	return Response.ok().entity(wishlist).build();
+	DocumentRepositoryClient client = new DocumentRepositoryClient(DocumentRepositoryClient.DEFAULT_BASE_URI);
+	final Response response = client.tenant(appAware.getHybrisTenant())
+		.appData(this.clientId)
+		.type(WISHLIST_PATH)
+		.dataId(wishlistId)
+		.prepareGet()
+		.withHeader("Authorization", authorization)
+		.execute();
+
+	return response;
+
     }
 
     /* PUT //{wishlistId} */
     public Response putByWishlistId(final AppAwareParameters appAware, final java.lang.String wishlistId,
-	    final Wishlist wishlist)
-    {
-	documentClient.updateWishlist(appAware.getHybrisTenant(), wishlist, wishlistId);
-	return Response.ok().build();
+	    final Wishlist wishlist) {
+	String authorization = oAuth2Client.requestAccessToken(appAware.getHybrisTenant());
+
+	DocumentRepositoryClient client = new DocumentRepositoryClient(DocumentRepositoryClient.DEFAULT_BASE_URI);
+	final Response response = client.tenant(appAware.getHybrisTenant())
+		.appData(this.clientId)
+		.type(WISHLIST_PATH)
+		.dataId(wishlistId)
+		.preparePut()
+		.withHeader("Authorization", authorization)
+		.withPayload(Entity.json(wishlist))
+		.execute();
+
+	return response;
     }
 
     /* DELETE //{wishlistId} */
-    public Response deleteByWishlistId(final AppAwareParameters appAware, final java.lang.String wishlistId)
-    {
-	documentClient.deleteWishlist(appAware.getHybrisTenant(), wishlistId);
-	return Response.noContent().build();
+    public Response deleteByWishlistId(final AppAwareParameters appAware, final java.lang.String wishlistId) {
+	String authorization = oAuth2Client.requestAccessToken(appAware.getHybrisTenant());
+
+	DocumentRepositoryClient client = new DocumentRepositoryClient(DocumentRepositoryClient.DEFAULT_BASE_URI);
+	final Response response = client.tenant(appAware.getHybrisTenant())
+		.appData(this.clientId)
+		.type(WISHLIST_PATH)
+		.dataId(wishlistId)
+		.prepareDelete()
+		.withHeader("Authorization", authorization)
+		.execute();
+
+	return response;
     }
 
 }
