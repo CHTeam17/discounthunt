a35c24aa33719c89b986d6aa23be6482a2980410
Sebastian Netuschil <sebastian.netuschil@sap.com>
2015-07-02 15:20:58 +0200

    change cf deployment

--- upload.sh
+++ upload.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 #Load properties file
-. ../user-team-sap-east.properties
-
 if [ $# == 0 ]
   then echo "Please provide path to manifest file"
 else
   export HTTPS_PROXY=http://proxy.wdf.sap.corp:8080
-  cf login -u $user -p $password -o $organisation -a $target --skip-ssl-validation
-  cf push -f $1
+  ./cf login -u $CF_USER -p $CF_PASSWORD -o sap_cec_prod -a https://api.cf3.hybris.com --skip-ssl-validation
+  ./cf push -f $1
 fi
